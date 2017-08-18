b89e8f68b77fa2ca3a4970ebafe9d0d9ea3d52fb
pkeogan <30511153+pkeogan@users.noreply.github.com>
2017-08-17 22:26:25 -0500

    Create DatatablesEditorServiceProvider.php

---
+++ src/DatatablesEditorServiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Pkeogan\DatatablesEditorEditorEditor;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class DatatablesEditorEditorServiceProvider.
+ *
+ * @package Pkeogan\DatatablesEditorEditorEditor
+ * @author  Arjay Angeles <aqangeles@gmail.com>
+ */
+class DatatablesEditorEditorServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Check if app uses Lumen.
+     *
+     * @return bool
+     */
+    protected function isLumen()
+    {
+        
+    }
+
+    /**
+     * Create aliases for the dependency.
+     */
+    protected function registerAliases()
+    {
+        
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return string[]
+     */
+    public function provides()
+    {
+
+    }
+}
